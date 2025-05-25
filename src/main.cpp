54f15cf0763f590bd7425a78466c0d6946b0ec1f
Justin Greer <justingreerbbi@users.noreply.github.com>
2025-05-25 15:04:44 -0400

    Basic Stripped down version

--- src/main.cpp
+++ src/main.cpp
@@ -8,25 +8,58 @@
 #include "system/settings.h"
 #include "tasks/task_app.h"
 #include "tasks/task_lora.h"
+#include <RF24.h>
+
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, RST_OLED_PIN);
 bool isTransmitting = false;
 
-extern void taskNRF24Handler(void*);
+// NRF24
+RF24 radio(38, 39);  // CE, CSN
 
 void setup() {
   Serial.begin(115200);
   while (!Serial);
 
-  pinMode(LED_PIN, OUTPUT);
-  loadSettings();
-  initDisplay(settings.deviceName, settings.frequency, settings.txPower);
-  initLoRa(settings.frequency, settings.txPower);
-  initGPS();
+  // Setup the RF24 Radio
+  SPI.begin(40, 42, 41, 39);  // SCK, MISO, MOSI, CSN
 
-  xTaskCreatePinnedToCore(taskLoRaHandler, "LoRaTask", 4096, NULL, 1, NULL, 1);
-  xTaskCreatePinnedToCore(taskAppHandler, "AppTask", 8192, NULL, 1, NULL, 0);
-  xTaskCreatePinnedToCore(taskNRF24Handler,"NRF24Task",4096, NULL,1,NULL, 1); // Run on core 1 with LoRa????
+  if (!radio.begin()) {
+    Serial.println("NRF|ERROR|INIT_FAIL");
+    while (1);
+  }
+
+  const byte address[6] = "00001";
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+
+  const char text[] = "Hello NRF24!";
+  if (radio.write(&text, sizeof(text))) {
+    Serial.println("NRF|INFO|SEND_OK");
+  } else {
+    Serial.println("NRF|ERROR|SEND_FAIL");
+  }
+
+  //pinMode(LED_PIN, OUTPUT);
+  //loadSettings();
+  
+  //initDisplay(settings.deviceName, settings.frequency, settings.txPower);
+  
+  //initLoRa(settings.frequency, settings.txPower);
+  
+  //initGPS();
+
+  //xTaskCreatePinnedToCore(taskLoRaHandler, "LoRaTask", 4096, NULL, 1, NULL, 1);
+  //xTaskCreatePinnedToCore(taskAppHandler, "AppTask", 8192, NULL, 1, NULL, 0);
+  //xTaskCreatePinnedToCore(taskNRF24Handler,"NRF24Task",4096, NULL,1,NULL, 1); // Run on core 1 with LoRa????
 }
 
-void loop() {}
+void loop() {
+  //if (radio.available()) {
+  //  char received[32] = {0};
+  //  radio.read(&received, sizeof(received));
+  //  Serial.print("NRF|INFO|RECEIVED|");
+  //  Serial.println(received);
+  //}
+}
